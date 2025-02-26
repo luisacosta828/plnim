7258afb524464c2349875eb6383ce3fdd98504cb
Luis Acosta <luisacosta828@gmail.com>
2025-02-26 14:47:31 -0400

    fix: cleaning plnim and adding validator function

--- src/plnim.nim
+++ src/plnim.nim
@@ -4,66 +4,121 @@ import pgxcrown/catalog/[pg_proc, pg_type]
 import pgxcrown/syscache
 import pgxcrown/spi
 import dynlib
-import std/strutils
+import std/[strutils, sequtils]
+import os, osproc
 
 PG_MODULE_MAGIC
 
-template getFnLanguage:Datum =
-  SysCacheGetAttr(PROCOID, heapTuple, Anum_pg_proc_prolang, addr(is_null))
+proc translate_pg_types_to_nim(typ: string): string {.inline.} =
+  case typ
+  of "int4": "int32"
+  of "int8": "int64"
+  of "float4": "float32"
+  of "float8": "float64"
+  else: "unknown"
 
-template getFnNArgs:uint16 =
-  DatumGetUInt16(SysCacheGetAttr(PROCOID, heapTuple, Anum_pg_proc_pronargs, addr(is_null)))
 
-template getFnSourceCode: cstring =
-  DatumGetCString(SysCacheGetAttr(PROCOID, heapTuple, Anum_pg_proc_prosrc, addr(is_null)))
+proc to_pgxcrown(proname: cstring, prosrc: cstring, pronargs: int16, proargtypes: ptr Oid, prorettype: Oid, proargnames: seq[string]): string =
+  var 
+    plnim_args:seq[cstring]
+    is_null = false
+    rettype_tuple =  SearchSysCache1(TYPEOID, ObjectIdGetDatum(prorettype))
+    plnim_rettype = get_pg_type_name(rettype_tuple)
 
-template getFnArgNames: Datum =
-  SysCacheGetAttr(PROCOID, heapTuple, Anum_pg_proc_proargnames, addr(is_null))
+  ReleaseSysCache(rettype_tuple) 
 
- 
+  for n in 0 ..< pronargs:
+    var 
+      oid_value    = (proargtypes + n).asOid
+      type_tuple   = SearchSysCache1(TYPEOID, ObjectIdGetDatum(oid_value))
+    
+    plnim_args.add get_pg_type_name(type_tuple)
+    ReleaseSysCache(type_tuple)
+
+  if len(plnim_args) == pronargs:
+    var proc_template = """
+proc $proc_name($args): $ret_type =
+$body
+"""
+    var args:seq[string]
+    for arg in zip(proargnames, plnim_args):
+      args.add arg[0] & ": " & translate_pg_types_to_nim($arg[1])
+
+    result = proc_template.multireplace([("$proc_name", $proname), ("$ret_type", translate_pg_types_to_nim($plnim_rettype)), ("$body", $prosrc), ("$args", args.join(", "))])
+    
+
+template run_command(command: string) =
+    discard execShellCmd(load_env.replace("$command", command))
+
+
+proc plnim_validator*(): Datum {. pgv1 .} =
+    var 
+      fn_oid = getOid(0)
+      heapTuple = SearchSysCache1(PROCOID, ObjectIdGetDatum(fn_oid)) 
+      is_null = false
+      proname = get_pg_proc_name(heapTuple) 
+      prosrc = get_pg_proc_src(heapTuple)
+      pronargs = get_pg_proc_nargs(heapTuple)
+      prodefault_nargs = get_pg_proc_default_nargs(heapTuple)
+      proargtypes = get_pg_proc_argtypes(heapTuple)
+      prorettype = get_pg_proc_rettype(heapTuple) 
+      proargnames = get_pg_proc_argnames(heapTuple)
+      
+    #Get source code from plnim function 
+    var code = to_pgxcrown(proname, prosrc, pronargs, proargtypes, prorettype, proargnames)
+
+    when defined(linux):
+      var
+        home = getCurrentDir() / ".." / ".." 
+        current_user = home.lastPathPart
+        pgxtool_init_dir = home / current_user & "_pgxtool"
+        pgxtool_bin = execCmdEx("echo $NIMPATH").output.strip
+        load_env = "/bin/bash -c 'export PATH=$PGXTOOL_DIR:$PATH;$command'".replace("$PGXTOOL_DIR", pgxtool_bin)
+    
+      if not dirExists(pgxtool_init_dir):
+        run_command("pgxtool init")
+       
+      var
+        prj_dir   = pgxtool_init_dir & "/$project_name/src" 
+      prj_dir   = prj_dir.replace("$project_name", $proname)
+
+      var main_file = prj_dir / "main.nim"
+      if not fileExists(main_file):
+        run_command("pgxtool create-project $name".replace("$name",$proname))
+      
+      writeFile(main_file, code)
+      
+      #build extension
+      run_command("pgxtool build-extension $fn".replace("$fn", $proname))
+
+    ReleaseSysCache(heapTuple)
+    return cast[Datum](0)
+
+
+
 proc plnim_call_handler*(fcinfo: FunctionCallInfo): Datum {.pgv1_plnim.} = 
   
     type pg_proc = proc(a: FunctionCallInfo): Datum {. nimcall .}
     
-    #Load plnim function oid and plnim oid
-    #and inject fn_oid and lang_datum varibles into this scope
-
     var 
       fn_oid = getFnOid(fcinfo)
       heapTuple = SearchSysCache1(PROCOID, ObjectIdGetDatum(fn_oid)) 
       is_null = false
-      pl_struct = cast[Form_pg_proc](getStruct(heapTuple))
-      proname = NameStr(pl_struct.proname) 
-      prosrc = getFnSourceCode
-    echo "!!"
-    echo "pl_struct.pronargs: ", $pl_struct.pronargs 
-    echo "pl_struct.prorettype: ", $pl_struct.prorettype
-    for n in 0 ..< pl_struct.pronargs:
+      proname = get_pg_proc_name(heapTuple)
+
+    when defined(linux):
       var 
-        argtype     = pl_struct.proargtypes.values[n]
-        type_tuple  = SearchSysCache1(TYPEOID, ObjectIdGetDatum(argtype))
-        type_struct = cast[Form_pg_type](getStruct(type_tuple))
+        libname = "/var/lib/postgresql/postgresql_pgxtool/$prj/src/$lib".replace("$prj", $proname).replace("$lib", $proname) 
+        lib = loadLib(libname)  
       
-      echo "argument data type: ", NameStr(type_struct.typname)
-      echo "type_tuple is valid: " & $IsValid(type_tuple)
+      ReleaseSysCache(heapTuple)   
+      var fn_call = cast[pg_proc](lib.symAddr("pgx_" & $proname))
+      return fn_call(fcinfo)
 
-      ReleaseSysCache(type_tuple)
-
-
-
-    #Get source code from plnim function 
-
-    #var source_info = getFunctionHeader(fn_oid, lang_datum)
-    #var lib = loadlib("lib"&source_info.func_name&".so")
-    #var pfun = lib.symAddr("pgx_" & source_info.func_name)
-
-    #if lib.isNil:
-    #    echo "no se pudo cargar la libreria: " & "lib" & source_info.func_name & ".so"
-    #else:
-    #    var fun = cast[pg_proc](pfun)
-    #    return fun(getFcinfoData())
-    ReleaseSysCache(heapTuple)   
-    returnInt32(-404)           
+    else:
+      ReleaseSysCache(heapTuple)   
+      returnInt32(-404)           
 
 PG_FUNCTION_INFO_V1(plnim_call_handler)
+PG_FUNCTION_INFO_V1(plnim_validator)
 
--- src/plnim/sql/extension.sql
+++ src/plnim/sql/extension.sql
@@ -1,11 +1,14 @@
 create function plnim_call_handler()
-returns language_handler as '$libdir/libplnim' 
+returns language_handler as '$libdir/plnim' 
 language c strict;
 
-create language plnim
-handler plnim_call_handler;
+create function plnim_validator(oid) 
+returns void as '$libdir/plnim'
+language c strict;
+
+create trusted language plnim
+handler plnim_call_handler
+validator plnim_validator;
 
 comment on language plnim
 is 'PL/Nim procedural language';
-
-
